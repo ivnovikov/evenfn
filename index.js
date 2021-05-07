f899f413bd137e1d49cdf20e8f7274e00909d864
Ilja Novikov <ivnovikov@luxoft.com>
2021-05-08 01:02:50 +0200

    no IIFE implementation

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const mod = (() => {
-  const DELAY = 5000;
-  const state = new Map();
+const f = function () {
+  this.state = new Map();
 
-  const getDelay = val => {
-    state.set(val, state.has(val) ? 0 : DELAY);
-    return state.get(val);
+  this.checkValue = async function (num) {
+    const current = {
+      value: num,
+      even: num % 2 === 0,
+      delay: this.state.has(num) ? 0 : 5000,
+    };
+    this.state.set(num, true);
+    return current;
   };
 
-  return {
-    isEven: num => {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve(num % 2 === 0);
-        }, getDelay(num));
-      });
-    },
+  this.isEven = async function (val) {
+    const result = await this.checkValue(val);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(result.even), result.delay);
+    });
   };
-})();
+};
 
 const main = async () => {
+  const mod = new f();
   console.log(await mod.isEven(1));
   console.log(await mod.isEven(1));
   console.log(await mod.isEven(1));
